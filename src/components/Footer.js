e93ecf1ce9889748a6b99547b5daeda32253bac3
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-06-10 13:46:59 +0530

    Update component - Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,10 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="bg-[#1A1919] text-white text-center py-6">
-      <p className="font-poppins font-semibold mb-3 text-sm">Copyright &copy; MoviePicker 2023</p>
-      <p className="font-mono text-sm">
+      <p className="font-poppins font-semibold mb-3 text-sm">Copyright &copy; MoviesPicker 2023</p>
+      <p className="font-firasans tracking-widest text-sm">
         This website is based on {" "}
-        <a href="https://developer.themoviedb.org/reference/intro/getting-started" target="_blank" rel='noreferrer' className="underline text-sky-400 font-semibold">
+        <a href="https://developer.themoviedb.org/reference/intro/getting-started" target="_blank" rel='noreferrer' className="underline text-sky-400 font-bold">
           TMDB API
         </a>
       </p>
