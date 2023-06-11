58c94e66d7544f2de811f37568bfdce8c91302ac
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-06-11 20:56:38 +0530

    Create search page

--- src/pages/Searchpage/index.js
+++ src/pages/Searchpage/index.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import SearchArea from "../../components/SearchArea";
+import MoviesList from "../../components/MoviesList";
 
-export default function Search() {
+export default function Searchpage() {
   return (
-    <div>index</div>
-  )
+    <div className="px-6 md:px-16 mt-16">
+      <SearchArea />
+      <h1 className="font-bold font-signika text-3xl mt-16">Search Result</h1>
+      <hr className="border mt-2 border-black/10" />
+      <MoviesList />
+    </div>
+  );
 }
