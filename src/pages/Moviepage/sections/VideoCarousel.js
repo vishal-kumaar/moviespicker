939c6be2e48ad11110a7acb13087780640c1fddf
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-06-12 22:15:53 +0530

    Create component - VideoPlayer

--- src/pages/Moviepage/sections/VideoCarousel.js
+++ src/pages/Moviepage/sections/VideoCarousel.js
@@ -14,7 +14,7 @@ export default function VideoCarousel() {
               className="bg-[url(https://i.ytimg.com/vi/yjRHZEUamCc/hqdefault.jpg)] flex-auto flex-shrink-0 flex-grow-0 h-60 w-96 bg-center bg-no-repeat bg-cover flex items-center justify-center"
             >
               <button className="bg-black/60 rounded-full h-14 w-14 hover:bg-black/60">
-                <div className="border-2 border-black h-full w-full rounded-full flex items-center justify-center hover:opacity-70 transition-all duration-200">
+                <div className="=h-full w-full rounded-full flex items-center justify-center hover:opacity-70 transition-all duration-200">
                   <img src={playIcon} alt="" className="invert w-4" />
                 </div>
               </button>
