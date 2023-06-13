e2796cecfdbcf0b57dd21e76cde600ae3ec90837
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-06-13 14:53:04 +0530

    Create component - PersonMovies

--- src/pages/Personpage/index.js
+++ src/pages/Personpage/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import PersonBio from './sections/PersonBio'
+import PersonMovies from './sections/PersonMovies'
 
 export default function Personpage() {
   return (
     <>
         <PersonBio />
+        <PersonMovies />
     </>
   )
 }
