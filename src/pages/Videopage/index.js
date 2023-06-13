27c98302df1e2c6411dc8f5e398c81298e5f3cc8
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-06-13 19:55:47 +0530

    Create component - Tab

---
+++ src/pages/Videopage/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Tab from "./section/Tab";
+import MovieHeader from "../../components/MovieHeader";
+
+export default function Videopage() {
+  return (
+    <>
+      <MovieHeader />
+      <Tab />
+    </>
+  );
+}
