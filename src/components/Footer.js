5ef9dce4b6a363d1f1bd34c8df50abebc98a5dfc
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-06-22 12:40:29 +0530

    Add personal website link in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import React from "react";
 export default function Footer() {
   return (
     <footer className="bg-[#1A1919] text-white text-center py-6">
-      <p className="font-poppins font-semibold mb-3 text-sm">
-        Copyright &copy; MoviesPicker {new Date().getFullYear()}
-      </p>
+    <p className="font-semibold mb-2 text-base">
+      <span className="font-signika">Made by</span> ❤ <span className="font-signika">with</span> <a className="text-blue-400 font-signika" href="https://vishalkumar.vercel.app" target="_blank" rel="noreferrer">Vishal</a>
+    </p>
       <p className="font-firasans tracking-widest text-sm">
         This website is based on{" "}
         <a
