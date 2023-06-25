94477e02f13f6140990d3837761eec6b49bf4f4c
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-06-25 17:39:39 +0530

    Update the movie hero bg

--- src/pages/Tvseriespage/sections/Hero.js
+++ src/pages/Tvseriespage/sections/Hero.js
@@ -3,7 +3,7 @@ import SearchArea from "../../../components/SearchArea";
 
 export default function Hero() {
   return (
-    <main className="bg-tvCover h-[40rem] w-full bg-fixed bg-no-repeat bg-cover bg-center mb-6">
+    <main className="bg-tvCover h-[40rem] w-full bg-fixed bg-no-repeat bg-cover mb-6">
       <div className="bg-black/60 h-[40rem] w-full px-10 sm:px-14 md:px-20 lg:px-32 py-28 md:py-40">
         <div>
           <h1 className="text-white text-7xl font-bold font-signika mb-3">
