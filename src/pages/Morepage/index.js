8821b6a1574649ec08709f60ac816ad145ec2335
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-02 21:57:15 +0530

    Fix pagination

--- src/pages/Morepage/index.js
+++ src/pages/Morepage/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Header from "./sections/Header";
 import ShowList from "./sections/ShowList";
 import { useSearchParams } from "react-router-dom";
@@ -6,6 +6,7 @@ import NotFound from "../../components/NotFound";
 
 export default function Morepage() {
   const [searchParams, setSearchParams] = useSearchParams();
+  const ref = useRef(false);
   let activeTab = searchParams.get("activeTab");
   if (!activeTab) {
     activeTab = "Movie";
@@ -13,8 +14,12 @@ export default function Morepage() {
 
   useEffect(
     () => {
-      searchParams.set("page", 1);
-      setSearchParams(searchParams, { replace: true });
+      if (ref.current) {
+        searchParams.set("page", 1);
+        setSearchParams(searchParams, { replace: true });
+      } else {
+        ref.current = true;
+      }
     },
     // eslint-disable-next-line
     [activeTab]
