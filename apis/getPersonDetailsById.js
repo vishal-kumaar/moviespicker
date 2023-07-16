1edeaf18a563526f9e22016ddd214b0b89b61a3f
Vishal Kumar <vishal.kumar08376@gmail.com>
2023-07-16 15:30:34 +0530

    Update getPersonDetailsById api to include images

--- apis/getPersonDetailsById.js
+++ apis/getPersonDetailsById.js
@@ -13,11 +13,16 @@ const getPersonDetailsById = async (personId) => {
       `https://api.themoviedb.org/3/person/${personId}?api_key=${process.env.NEXT_PUBLIC_API_KEY}&language=en-US&append_to_response=tv_credits`
     );
 
+    const res4 = await axios.get(
+      `https://api.themoviedb.org/3/person/${personId}/images?api_key=${process.env.NEXT_PUBLIC_API_KEY}`
+    );
+
     return {
       data: {
         person: res1.data,
         movies: res2.data,
         series: res3.data.tv_credits.cast,
+        images: res4.data.profiles,
       },
       success: true,
     };
